0f4e61ec8abe262f620e91790dc9ddc73a6b3d21
Paulina <paulinashypulia@gmail.com>
2024-09-10 20:01:57 +0400

    'homework_4_done'

---
+++ homework/paulina_sivazhalezava/homework4/data_types_task.py
@@ -0,0 +1,14 @@
+my_dict = {'tuple': ('cat1', 'cat2', 'cat3', 'cat4', 'cat5'),
+           'list': [4, 5, 3, 4, 8],
+           'dict': {'dog1': 'Rex', 'dog2': 'Suslik', 'dog3': 'Aku', 'dog4': 'Hunter', 'dog5': 'Volchok'},
+           'set': {1.1, 1.2, 1.3, 1.4, 1.5}}
+
+print('last element of tuple :', my_dict['tuple'][-1])
+my_dict['list'].append(10)
+my_dict['list'].pop(1)
+my_dict['dict']['I am a tuple,'] = (1, 2)
+my_dict['dict'].__delitem__('dog3')
+my_dict['set'].add(1.6)
+my_dict['set'].remove(1.3)
+
+print(my_dict)
