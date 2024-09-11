da6d2acccfce7cd805fcbf937686f4917537aec4
Paulina <paulinashypulia@gmail.com>
2024-09-11 17:29:14 +0400

    'homework_4_fixed_tuple_key'

--- homework/paulina_sivazhalezava/homework4/data_types_task.py
+++ homework/paulina_sivazhalezava/homework4/data_types_task.py
@@ -6,7 +6,7 @@ my_dict = {'tuple': ('cat1', 'cat2', 'cat3', 'cat4', 'cat5'),
 print('last element of tuple :', my_dict['tuple'][-1])
 my_dict['list'].append(10)
 my_dict['list'].pop(1)
-my_dict['dict']['I am a tuple,'] = (1, 2)
+my_dict['dict'][('i am a tuple',)] = 1
 my_dict['dict'].__delitem__('dog3')
 my_dict['set'].add(1.6)
 my_dict['set'].remove(1.3)
