21f0e23e0f29fb5d852f9e4bfa1948b5c1f0e871
Paulina <paulinashypulia@gmail.com>
2024-09-14 11:43:18 +0400

    'corrected_search_by_index_in_task2'

--- homework/paulina_sivazhalezava/homework5/strings_task2.py
+++ homework/paulina_sivazhalezava/homework5/strings_task2.py
@@ -1,12 +1,11 @@
 result1 = 'результат операции: 42'
 result2 = 'результат операции: 514'
 result3 = 'результат работы программы: 9'
-number_to_add = 10
 
 
-number1 = int(result1[result1.index('42'):]) + number_to_add
-number2 = int(result2[result2.index('514'):]) + number_to_add
-number3 = int(result3[result3.index('9'):]) + number_to_add
+number1 = int(result1[result1.index(':') + 1:].strip()) + 10
+number2 = int(result2[result2.index(':') + 1:].strip()) + 10
+number3 = int(result3[result3.index(':') + 1:].strip()) + 10
 
 
 print(number1, number2, number3)
