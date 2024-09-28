84a26c228a1f669cd95049f4a29a753468f125a8
Paulina <paulinashypulia@gmail.com>
2024-09-28 18:50:35 +0400

    'fixed_lint_issue'

--- homework/paulina_sivazhalezava/homework9/map_task2.py
+++ homework/paulina_sivazhalezava/homework9/map_task2.py
@@ -4,4 +4,4 @@ temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 
 hot_days_summer = list(filter(lambda temperature: temperature > 28, temperatures))
 
 print(hot_days_summer)
-print(min(hot_days_summer), max(hot_days_summer), round(sum(hot_days_summer)/len(hot_days_summer)))
+print(min(hot_days_summer), max(hot_days_summer), round(sum(hot_days_summer) / len(hot_days_summer)))
